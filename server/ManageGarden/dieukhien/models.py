f22ac499d47cd7d089b3896929808b97a33d28f7
batchanhdao <anhle12341710@gmail.com>
2024-04-15 08:57:22 +0700

    models dieu khien

--- server/ManageGarden/dieukhien/models.py
+++ server/ManageGarden/dieukhien/models.py
@@ -5,6 +5,17 @@ from datetime import time
 # Create your models here.
 class DieuKhien(models.Model):
     manhdat = models.OneToOneField(ManhDat, models.SET_NULL, null=True)
+
+    lamp_time_on = models.TimeField(default=time(18,00,00))
+    lamp_time_off = models.TimeField(default=time(6,00,00))
+    water_pump_on = models.IntegerField(default=50)
+    water_pump_off = models.IntegerField(default=70)
+    fan_on = models.IntegerField(default=35)
+    fan_off = models.IntegerField(default=25)
+    sun_roof_time_close = models.TimeField(default=time(11,00,00))
+    sun_roof_time_open = models.TimeField(default=time(14,00,00))
+    created_at = models.DateTimeField(auto_now_add=True)
+
     # each individual status
     # TUOI_NUOC = 1
     # BAT_DEN_CHIEU_SANG = 2
@@ -17,16 +28,6 @@ class DieuKhien(models.Model):
     #             (BAT_QUAT_LAM_MAT, 'BAT_QUAT_LAM_MAT'),
     #                   (MO_MAY_CHE, 'MO_MAY_CHE'),
     #             (DONG_MAY_CHE, 'DONG_MAY_CHE'),)
-    lamp_time_on = models.TimeField(default=time(18,00,00))
-    lamp_time_off = models.TimeField(default=time(6,00,00))
-    water_pump_on = models.IntegerField(default=50)
-    water_pump_off = models.IntegerField(default=70)
-    fan_on = models.IntegerField(default=35)
-    fan_off = models.IntegerField(default=25)
-    sun_roof_time_close = models.TimeField(default=time(11,00,00))
-    sun_roof_time_open = models.TimeField(default=time(14,00,00))
-    created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return self.str(manhdat)
 
