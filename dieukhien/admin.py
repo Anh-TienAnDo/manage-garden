4fb6e20d2f1cdeac02743f12ab082d86e4ee8738
batchanhdao <lea81807@gmail.com>
2024-05-12 20:58:27 +0700

    anh le

---
+++ dieukhien/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+db_name = "default"
+class DieuKhienAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save(using=db_name)
+    def get_queryset(self, request):
+        return DieuKhien.objects.using(db_name)
+
+admin.site.register(DieuKhien, DieuKhienAdmin)
